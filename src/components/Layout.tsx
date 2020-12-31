ffce72c6e123bf11e50ef904d649b3cceb385111
Doug Hooker <DouglassHooker@gmail.com>
2020-12-30 22:56:19 -0600

    Add an example tailwind styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,11 +4,20 @@ import { Home } from './Home';
 
 export const Layout: React.FC = () => {
   return (
-    <main className="p-5">
+    <main>
       <nav></nav>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="*" component={() => <h1>404 - Page not found</h1>} />
+        <Route
+          path="*"
+          component={() => {
+            return (
+              <div className="p-5">
+                <h1 className="text-2xl">404 - Page not found</h1>
+              </div>
+            );
+          }}
+        />
       </Switch>
     </main>
   );
